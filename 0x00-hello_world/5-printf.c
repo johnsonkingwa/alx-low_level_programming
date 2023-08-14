1785c0cdf4b9f708dbbe6f43dd60da25c640176c
johnsonkingwa <johnsonkingwa69@gmail.com>
2023-08-14 02:18:14 -0700

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,13 +1,11 @@
 #include <stdio.h>
-
 /**
  * main - this is the main function
  *
- * Return: o when successfull
- * /
-
+ * Return: 0 when successfull
+ */
 int main(void)
 {
 printf("with proper grammar, but the outcome is a piece of art,");
-return (o);
+return (0);
 }
